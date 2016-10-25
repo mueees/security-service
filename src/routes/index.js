a4a0e5eec581d28a414167cca8e4db10422cfa44
mue <mue.miv@gmail.com>
2016-10-25 21:42:30 +0300

    add signin post API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,42 @@
 'use strict';
 
+let _ = require('lodash');
+
 let error = require('mue-core/modules/error');
+let utils = require('mue-core/modules/utils');
+
+// add necessary actions
+let action = require('mue-core/modules/action');
+require('mue-core/modules/actions/request-to-service');
+
+let trustedHostMiddleware = require('../middlewares/trusted-host');
+
 const API_PREFIX = '/api';
 
+function isUserDataValid(userData) {
+    return utils.isStringWithLength(userData.password) && utils.isEmail(userData.email);
+}
+
 module.exports = function (app) {
-    app.get(API_PREFIX + '/info', function (request, response, next) {
-        next(error.getHttpError(400, 'Cannot parse that'));
+    app.post(API_PREFIX + '/signin', trustedHostMiddleware, function (request, response, next) {
+        let userData = _.pick(request.body, ['email', 'password']);
+
+        if (isUserDataValid(userData)) {
+            action.execute('requestToService', {
+                service: 'account',
+                method: 'POST',
+                url: '/isValidCredential',
+                data: {
+                    email: userData.email,
+                    password: userData.password
+                }
+            }).then(function () {
+                response.send();
+            }).catch(function (err) {
+                next(error.getHttpError(400, err.message));
+            });
+        } else {
+            error.getHttpError(400, 'Invalid email or password');
+        }
     });
 };
